2fd6e15b9fd3209ba008cdbff79cb707f53118b0
Bruno Barreira <brunobarreira@MacBook-Pro-de-Bruno-2.local>
2018-08-13 18:43:20 -0300

    Recebendo requisições através do insomnia com bodyParser

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 const app = require('express')();
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
+const bodyParser = require('body-parser');
 
 const dbConfig = require('./config/database');
 
 mongoose.connect(dbConfig.url);
 requireDir(dbConfig.modelsPath);
 
-const User = mongoose.model('User');
-User.create({
-  name: 'Diego',
-  username: 'diego3m',
-  email: 'diego@rocketseat.com',
-  password: '123456',
-}, () => {
-  console.log('OK');
+app.use(bodyParser.json());
+
+app.post('/create', async (req, res) => {
+  const User = mongoose.model('User');
+
+  console.log(req.body);
+
+  await User.create(req.body);
+
+  return res.send();
 });
 
 app.listen(3000);
