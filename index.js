e2d60c0576f74465cb62b0a8770c712776d1c7b3
barreirabruno <brunobarreirasilva@gmail.com>
2018-08-14 04:36:04 -0300

    Estrutura de criacao de usuário está pronta

--- index.js
+++ index.js
@@ -10,14 +10,6 @@ requireDir(dbConfig.modelsPath);
 
 app.use(bodyParser.json());
 
-app.post('/create', async (req, res) => {
-  const User = mongoose.model('User');
-
-  console.log(req.body);
-
-  await User.create(req.body);
-
-  return res.send();
-});
+app.use('/api', require('./app/routes'));
 
 app.listen(3000);
