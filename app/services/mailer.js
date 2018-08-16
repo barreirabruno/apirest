41567ae0e2cbce70571a230e2be2c859f5a2ca71
Bruno Barreira <brunobarreira@MacBook-Pro-de-Bruno-2.local>
2018-08-16 18:51:11 -0300

    Envio de email após cadastro de usuário funcionando, arrumar o arquivo de enviorment

--- app/services/mailer.js
+++ app/services/mailer.js
@@ -1,15 +1,18 @@
 const nodemailer = require('nodemailer');
-const {
-  host,
-  port,
-  user,
-  pass,
-} = require('../../config/mail');
+// const {
+//   host,
+//   port,
+//   user,
+//   pass,
+// } = require('../../config/mail');
 
 const transport = nodemailer.createTransport({
-  host,
-  port,
-  auth: { user, pass },
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  auth: {
+    user: '358ad6e2d0fb1d',
+    pass: '234bf1410386af',
+  },
 });
 
 module.exports = options => transport.sendMail(options);
--- config/mail.js
+++ config/mail.js
@@ -1,11 +1,12 @@
-module.eports = {
+// module.eports = {
 
-  /**
-   * Auth
-   */
-  // Qual o host do smtp, usuario, senha, etc
-  host: process.env.MAIL_HOST,
-  port: process.env.MAIL_PORT,
-  user: process.env.MAIL_USER,
-  pass: process.env.MAIL_PASS,
-};
+//   /**
+//    * Auth
+//    */
+//   // Qual o host do smtp, usuario, senha, etc
+//   // host: process.env.MAIL_HOST,
+//   host: 'smtp.mailtrap.io',
+//   port: process.env.MAIL_PORT,
+//   user: process.env.MAIL_USER,
+//   pass: process.env.MAIL_PASS,
+// };
